50af154dcb3c313e316116c15a207b1f99bb4c71
Tashydaije <mbuguanatasha01@gmail.com>
2023-11-16 14:34:53 -0800

    add func to deallocate memory

--- free_mem.c
+++ free_mem.c
@@ -8,5 +8,77 @@
 
 void free_args(void)
 {
+	if (!_args)
+		return;
+
+	if (_args->opcode)
+	{
+		free(_args->opcode);
+		_args->opcode = NULL;
+	}
+	free_head();
+	if (_args->line)
+	{
+		free(_args->line);
+		_args->line = NULL;
+	}
 	free(_args);
+}
+
+/**
+ * free_tokens - Deallocates memory of tokens
+ *
+ * Return: void
+ */
+
+void free_tokens(void)
+{
+	int i;
+
+	if (!_args->tokens)
+		return;
+
+	for (i = 0; i < _args->num_tokens; i++)
+	{
+		free(_args->tokens[i]);
+	}
+	free(_args->tokens);
+	_args->tokens = NULL;
+}
+
+/**
+ * free_head - Deallocates memory of head
+ *
+ * Return: void
+ */
+
+void free_head(void)
+{
+	if (_args->head)
+	{
+		free_stack(_args->head);
+		_args->head = NULL;
+	}
+}
+
+/**
+ * free_stack - Deallocates memory of stack
+ * @head: The head of the stack
+ *
+ * Return: void
+ */
+
+void free_stack(stack_t *head)
+{
+	stack_t *temp;
+
+	if (!head)
+		return;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
